3a2e5823716119db0fd7ba53563f04d463d3a2fa
kkl <asd859999@gmail.com>
2021-08-16 21:40:10 +0800

    最後的教學 製作一個備忘錄

---
+++ script/htmlDom.js
@@ -0,0 +1,22 @@
+//window.alert("")
+// document
+
+window.addEventListener('load', function(params) {
+    const p1 = document.getElementById('title');
+    console.log(p1);
+    p1.innerText = "訂閱我"
+
+    const b1 = document.getElementById('button');
+    b1.addEventListener('click', function() {
+        console.log('點下去');
+    });
+
+    const box1 = document.getElementById('box');
+    box1.innerHTML = '<p>text</p>'
+
+    const in1 = document.getElementById('text')
+    in1.addEventListener('keyup', function(e) {
+        console.log('input')
+        console.log('e.target.value', e.target.value)
+    })
+})
